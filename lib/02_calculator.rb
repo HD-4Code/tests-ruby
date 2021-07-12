8f7fe74b2ac73f3d1ab6190a0401ba07e0bdece6
HD4Code <h.djivandjy@gmail.com>
2021-07-12 23:48:14 +0200

    Adding left files

---
+++ lib/02_calculator.rb
@@ -0,0 +1,19 @@
+def add (sprintf)
+    return ("%02d", 1)
+ end
+ 
+ def 
+ 2.step(a, b) { |i| puts i }
+ end
+ 
+ def addition(a,b)
+   puts a + b
+ end
+ 
+ def soustraction (a,b)
+    return a - b
+ end
+ 
+ def multiplication (a,b)
+    return a * b
+ end
